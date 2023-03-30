c90ac0f916db4e0520e1f176cbb639a06a5ffaf3
Max Cook <maxtcook@gmail.com>
2023-03-30 16:08:01 -0400

    Change statusline to Bidi-Mode indicator

--- test/init.test.lua
+++ test/init.test.lua
@@ -1,3 +1,7 @@
 -- Load bidi.nvim
 vim.opt.runtimepath:append('~/code/bidi.nvim')
-require('bidi').setup()
+
+local bidi = require('bidi')
+bidi.setup()
+
+vim.o.statusline = [[%!luaeval('require("bidi").buf_get_bidi_mode(vim.api.nvim_win_get_buf(0))')]]
